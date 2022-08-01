6847a9f8390185aeecf618c2a5d2090d7d22cc4e
huseyin <huseyinarslan06@gmail.com>
2022-08-01 11:23:18 +0300

    id

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,22 +1,18 @@
-
-
 const Card = (data) => {
-    console.log("ne geliyo", data);
-    const {id,title,image,desc} = data;
-    return(
-        <div className="cards">
-            <div className="title">
-                <h1>{title}</h1>
-            </div>
-            <img src={image} alt="" />
+  //   console.log("what is", data);
+  const { title, image, desc } = data;
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <img src={image} alt="" />
 
-            <div className="card-over">
-                <p>
-                    {desc}
-                </p>
-            </div>
-        </div>
-    )
-}
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
